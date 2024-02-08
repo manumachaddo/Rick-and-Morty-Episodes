3e932cd016dfe0c785520b137a74b5dc89de8dca
Emanuelle <emanuelle.machado@roost.com>
2024-02-08 14:50:06 -0300

    rick and morty project

--- src/app/rickandmorty/hook.tsx
+++ src/app/rickandmorty/hook.tsx
@@ -1,12 +1,54 @@
-import { useEffect, useState, useCallback } from "react";
-import type {Info, Episode} from "./interfaces";
-import { useInfiniteQuery } from "react-query";
+"use client";
+import { useEffect, useState } from "react";
+import type { BaseSearchResponde, Info, Episode } from "./interfaces";
+import axios from "axios";
 
-type Endpoints = {
-    episodes: Episode;
-    info: Info;
-}
+const useRickAndMortyAPI = () => {
+  const [data, setData] = useState<Episode[]>([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(false);
+  const [hasPrevPage, setHasPrevPage] = useState(false);
+  const numPages = 3;
 
-const useRickAndMortyAPI = <T extends keyof Endpoints>(endpoint: T) => {
-    
-}  
+  let apiUrl = `https://rickandmortyapi.com/api/episode?page=${currentPage}`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<BaseSearchResponde<Episode>>(apiUrl);
+        setData(response.data.results);
+        setHasNextPage(response.data.info.next !== null);
+        setHasPrevPage(currentPage > 1);
+      } catch (error) {
+        setError(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [apiUrl, currentPage]);
+
+  const goNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const goPrevPage = () => {
+   setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  return{
+      data,
+      error,
+      loading,
+      currentPage,
+      hasNextPage,
+      hasPrevPage,
+      goNextPage,
+      goPrevPage,
+  };
+
+};
+
+export default useRickAndMortyAPI
---
+++ src/app/rickandmorty/page.tsx
@@ -0,0 +1,74 @@
+"use client";
+import React from "react";
+import useRickAndMortyAPI from "./hook";
+import { Episode, Info } from "./interfaces";
+import Image from "next/image";
+import arrow from "./arrow.png";
+import flecha from "./flecha.png";
+
+const Page = () => {
+  const {
+    data,
+    error,
+    loading,
+    currentPage,
+    hasNextPage,
+    hasPrevPage,
+    goNextPage,
+    goPrevPage,
+  } = useRickAndMortyAPI();
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Erro: {error}</p>;
+  }
+
+  return (
+    <div>
+      <div className="flex pl-44 mt-4 mb-4">
+        <h1>Rick and Morty Episodios</h1>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th className="flex justify-start">Nome</th>
+            <th>Data que foi exibido</th>
+            <span style={{ paddingRight: "10px" }}></span>
+            <th>Episodio</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((episode: Episode) => (
+            <tr key={episode.id}>
+              <td>{episode.name}</td>
+              <td>{episode.air_date}</td>
+              <span style={{ paddingRight: "30px" }}></span>
+              <td>{episode.episode}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="mt-4">
+        {hasPrevPage && (
+          <button onClick={goPrevPage}>
+            Prev
+            <Image src={flecha} alt="flecha" width={20} />
+          </button>
+        )}
+
+        <span style={{ paddingRight: "500px" }}></span>
+        {hasNextPage && (
+          <button onClick={goNextPage}>
+            Next
+            <Image src={arrow} alt="flecha" width={20} />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
